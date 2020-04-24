f50a6e50c274bb56e9a4fdf7618248bfdff734c5
Mladen <mladen@Mladens-MacBook-Pro.local>
2020-04-24 23:41:42 +0200

    added responsive mode

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ document.body.addEventListener('wheel', (event) => {
     } else if (event.deltaY > 0) {
         document.querySelector("header").style.top = "-100px";
     }
-})
+});
+
+let ul = document.querySelector('.header-navigation-nav-links');
+
+document.getElementById('check').addEventListener('click', () => {
+    ul.classList.toggle('active');
+});
